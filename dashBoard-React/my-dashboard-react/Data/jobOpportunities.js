a3a9107f0483f17af1b34141a4cd0d6d6427cb38
OmKapadia2512 <om.kapadia@zeuslearning.com>
2024-02-06 10:55:49 +0530

    setup done

---
+++ dashBoard-React/my-dashboard-react/Data/jobOpportunities.js
@@ -0,0 +1,72 @@
+const jobOpportunities = [
+    {
+      title: "Walk In for Designer Job Role",
+      startDate: "10-jul-2021",
+      endDate: "11-jul-2021",
+      location: "Mumbai",
+      jobRoles: [
+        {
+          roleTitle: "Instructional Designer",
+          ctc: "$90,000",
+          roleDescription: "Develop user interfaces using modern frontend technologies",
+          requirements: ["React.js", "JavaScript", "HTML", "CSS"],
+        }
+      ],
+      timeSlots: ["Morning", "Afternoon", "Evening"],
+      preferences: ["Flexible Work Hours", "Remote Work Option"],
+    },
+    {
+      title: "Walk In for Multiple Job Roles",
+      startDate: "03-jul-2021",
+      endDate: "04-Jul-2021",
+      location: "Mumbai",
+      jobRoles: [
+        {
+          roleTitle: "Instructional Designer",
+          ctc: "$120,000",
+          roleDescription: "Develop and implement machine learning models",
+          requirements: ["Python", "TensorFlow", "Scikit-Learn"],
+        },
+        {
+          roleTitle: "Software Engineer",
+          ctc: "$80,000",
+          roleDescription: "Analyze and interpret complex data sets",
+          requirements: ["SQL", "Excel", "Statistical Analysis"],
+        },
+        {
+            roleTitle: "Software Quality Engineer",
+            ctc: "$80,000",
+            roleDescription: "Analyze and interpret complex data sets",
+            requirements: ["SQL", "Excel", "Statistical Analysis"],
+          }
+      ],
+      timeSlots: ["Flexible"],
+      preferences: ["Remote Work", "Health Insurance"],
+    },
+    {
+      title: "Walk In for Design and Development Job Role",
+      startDate: "10-Jul-2021",
+      endDate: "11-Jul-2021",
+      location: "Mumbai",
+      jobRoles: [
+        {
+            roleTitle: "Instructional Designer",
+            ctc: "$120,000",
+            roleDescription: "Develop and implement machine learning models",
+            requirements: ["Python", "TensorFlow", "Scikit-Learn"],
+          },
+          {
+            roleTitle: "Software Engineer",
+            ctc: "$80,000",
+            roleDescription: "Analyze and interpret complex data sets",
+            requirements: ["SQL", "Excel", "Statistical Analysis"],
+          },
+
+      ],
+      timeSlots: ["Flexible", "Part-Time"],
+      preferences: ["Remote Work", "Design Thinking"],
+    },
+  ];
+  
+  module.exports = jobOpportunities;
+  
---
+++ dashBoard-React/my-dashboard-react/components/Registration/Registration.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Registration = () => {
+  return (
+    <div>
+      Hello Registration
+    </div>
+  )
+}
+
+export default Registration
