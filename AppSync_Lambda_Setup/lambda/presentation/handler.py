d13078239baadf8a5c6256e20f50e3ee14cfa509
OmKapadia2512 <om.kapadia@zeuslearning.com>
2024-02-23 19:24:02 +0530

    backEnd to display List of Drive having JobRole and details of JobRole

--- AppSync_Lambda_Setup/lambda/presentation/handler.py
+++ AppSync_Lambda_Setup/lambda/presentation/handler.py
@@ -1,7 +1,7 @@
 def build_response(status_code, body):
     return {
         "statusCode": status_code,
-        "body": json.dumps(body),
+        "body": json.dumps(body, default=str),
         "headers": {"Content-Type": "application/json"},
     }
 
@@ -59,25 +59,70 @@ def handle_each_request(event):
             cursor = connection.cursor()
             print("3")
 
-            cursor.execute('SELECT * FROM "walk_in_drive" ')
-            results = cursor.fetchall()
-            print("hiiii")
-            print(results)
-            data = [
-                {
-                    "id": row[0],
-                    "guid": row[1],
-                    "drive_title": row[2],
-                    "start_date": str(row[3]),
-                    "end_date": str(row[4]),
-                    "location": row[5],
-                    "dt_created": str(row[6]),
-                    "dt_modified": str(row[7]),
+            drive_query = """
+                            SELECT id, guid, drive_title, dt_created, dt_modified, start_date, end_date, location
+                            FROM walk_in_drive
+                        """
+            job_title_query = """
+                                SELECT jr.job_title,jr.package,jr.job_description,job_requirements
+                                FROM job_role jr
+                                JOIN drive_has_job_role dhjr ON jr.id = dhjr.job_role_id
+                                WHERE dhjr.drive_id = %s
+                            """
+            time_slot_query = """
+                                SELECT ts.slot_timings
+                                FROM time_slot ts
+                                JOIN drive_has_time_slot dhts ON ts.id = dhts.time_slot_id
+                                WHERE dhts.drive_id = %s
+                                """
+
+            cursor.execute(drive_query)
+            drives = cursor.fetchall()
+            cursor.close()
+            
+            results = []
+
+
+            for drive in drives:
+                drive_id = drive[0]
+                
+                cursor = connection.cursor()
+                cursor.execute(job_title_query, (drive_id,))
+                job_details = []
+
+                for row in cursor.fetchall():
+                    job_details.append({
+                        'job_title': row[0],
+                        'package': row[1],
+                        'job_description': row[2],
+                        'job_requirements': row[3]
+                    })
+                cursor.execute(time_slot_query, (drive_id,))
+                time_slots = [row[0] for row in cursor.fetchall()]
+                cursor.close()
+                result = {
+                    'id': drive[0],
+                    'guid': drive[1],
+                    'drive_title': drive[2],
+                    'dt_created': drive[3],
+                    'dt_modified': drive[4],
+                    'start_date': drive[5],
+                    'end_date': drive[6],
+                    'location': drive[7],
+                    'job_Roles': job_details,
+                    'time_Slots': time_slots
                 }
-                for row in results
-            ]
+                results.append(result)
+
+            # Print the results
+            for result in results:
+                print(result)
+
+            # Close the connection
+            cursor.close()
+
         finally:
             cursor.close()
             close_db_connection(connection)
 
-    return build_response(HTTPStatus.OK, data)
+    return build_response(HTTPStatus.OK, results)
